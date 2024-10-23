48bf84ecab1421d1cc3edeb5da593561b07e2c84
Austin <austin@austinlong.dev>
2024-10-23 15:51:53 -0500

    Finished Med-26

--- Austin/med-26.py
+++ Austin/med-26.py
@@ -3,8 +3,12 @@ def balanced_paren(string):
     for el in string:
         if el == "(":
             current_level += 1
-        elif current_level == ")":
+        elif el == ")":
             current_level -= 1
         if current_level < 0:
             return False
     return current_level == 0
+
+print(balanced_paren("(())"))
+print(balanced_paren("())"))
+print(balanced_paren(")(())("))

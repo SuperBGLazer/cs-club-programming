01b7d7dfcee5459823f3bc50680bf920593e4a71
Trenton_Gibson <122380247+Trenton-Gibson@users.noreply.github.com>
2024-10-23 16:34:54 -0500

    Exponentation for Stupid Long

--- Trenton_Gibson/Add Two Numbers.py
+++ Trenton_Gibson/Add Two Numbers.py
@@ -3,15 +3,34 @@ import random
 random_num=0
 random_num2=0
 
+random_word=""
+random_word2=""
+
 count=0
 char=''
 while count< 10:
 	char=random.randrange(0,256)
 	if char%2==0:
 		count+=1
-		
+	char = str(char)
+	random_word+=char
 
+print(random_word)
+count=0
+while count< 10:
+	char=random.randrange(0,256)
+	if char%2==0:
+		count+=1
+	char=str(char)
+	random_word2+=char
 
+print(random_word2)
+random_num=int(random_word)**int(random_word2)
+random_num2=int(random_word)/int(random_word2)
+print(random_num)
+print(random_num2)
+
+print(random_num+random_num2)
 # random_num=chr(random_num)
 # random_num2=chr(random_num2)
 #
